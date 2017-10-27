19e47e64182a0970a569370cd9ebab08f3d9dc12
Morten Stulen <enstulen@gmail.com>
2017-10-27 19:52:58 +0200

    Server/client folder and create simple routing

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,30 @@
+// Modules
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {HttpModule} from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { ToplistComponent } from './components/toplist/toplist.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+
+const appRoutes: Routes = [
+  {path: '', component: ToplistComponent},
+  {path: 'categories', component: CategoriesComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ToplistComponent,
+    CategoriesComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
