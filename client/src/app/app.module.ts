579f324ce7318f6b637e122fe4fd111bc3594897
Morten Stulen <enstulen@gmail.com>
2017-10-28 18:43:10 +0200

    Finish alcohol items and infinite scroll

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import {HttpModule} from '@angular/http';
 import {RouterModule, Routes} from '@angular/router';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
+// Components
 import { AppComponent } from './app.component';
 import { ToplistComponent } from './components/toplist/toplist.component';
 import { CategoriesComponent } from './components/categories/categories.component';
 import { AlcoholItemComponent } from './components/alcohol-item/alcohol-item.component';
+import { ShowItemComponent } from './components/show-item/show-item.component';
+
+// Services
+import {DataService} from './services/data.service';
 
 const appRoutes: Routes = [
   {path: '', component: ToplistComponent},
-  {path: 'categories', component: CategoriesComponent}
+  {path: 'categories', component: CategoriesComponent},
+  {path: 'item/:id', component: ShowItemComponent}
+
 ];
 
 @NgModule({
@@ -20,15 +28,17 @@ const appRoutes: Routes = [
     AppComponent,
     ToplistComponent,
     CategoriesComponent,
-    AlcoholItemComponent
+    AlcoholItemComponent,
+    ShowItemComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
+    InfiniteScrollModule,
     NgbModule.forRoot(),
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- client/src/app/components/toplist/toplist.component.ts
+++ client/src/app/components/toplist/toplist.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AlcoholItemComponent } from '../alcohol-item/alcohol-item.component';
 import { Item } from '../../interfaces/item';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-toplist',
@@ -9,53 +10,26 @@ import { Item } from '../../interfaces/item';
 })
 export class ToplistComponent implements OnInit {
   items: [Item];
+  scrollNumber: number;
 
-  constructor() { }
+  onScroll() {
+    this.dataService.getTopListItems(this.scrollNumber).subscribe(items => {
+      for (let i = 0; i < items.length; i++) {
+        this.items.push(items[i]);
+      }
+      this.scrollNumber += 1;
+    });
+
+  }
+
+  constructor(private dataService: DataService) {
+    this.scrollNumber = 1;
+  }
 
   ngOnInit() {
-    // this.dataService.getPosts().subscribe(items => {
-    //   console.log(items);
-    //   this.items = items;
-    // });
-    this.items = [{
-      id: 1254,
-      name: 'Testing name1',
-      number: 45,
-      abv: 20,
-      apk: 2.03,
-      price: 299,
-      containerSize: 2,
-      points: 60
-    }, {
-      id: 8945,
-      name: 'Testing name2',
-      number: 45,
-      abv: 20,
-      apk: 2.03,
-      price: 299,
-      containerSize: 2,
-      points: 60
-    },
-    {
-      id: 2345,
-      name: 'Testing name3',
-      number: 45,
-      abv: 20,
-      apk: 2.03,
-      price: 299,
-      containerSize: 2,
-      points: 60
-    },
-    {
-      id: 2112,
-      name: 'Testing name4',
-      number: 45,
-      abv: 20,
-      apk: 2.03,
-      price: 299,
-      containerSize: 2,
-      points: 60
-    }];
+    this.dataService.getTopListItems(0).subscribe(items => {
+      this.items = items;
+    });
   }
 
 }
