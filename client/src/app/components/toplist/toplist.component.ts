c1a7522f02f43fca220ce520414edf21bc858221
Morten Stulen <enstulen@gmail.com>
2017-11-04 16:44:03 +0100

    Update API on toplist and categories

--- client/src/app/components/toplist/toplist.component.ts
+++ client/src/app/components/toplist/toplist.component.ts
@@ -1,35 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { AlcoholItemComponent } from '../alcohol-item/alcohol-item.component';
-import { Item } from '../../interfaces/item';
-import { DataService } from '../../services/data.service';
+import { Component, OnInit } from "@angular/core";
+import { AlcoholItemComponent } from "../alcohol-item/alcohol-item.component";
+import { Item } from "../../interfaces/item";
+import { DataService } from "../../services/data.service";
 
 @Component({
-  selector: 'app-toplist',
-  templateUrl: './toplist.component.html',
-  styleUrls: ['./toplist.component.css']
+	selector: "app-toplist",
+	templateUrl: "./toplist.component.html",
+	styleUrls: ["./toplist.component.css"]
 })
 export class ToplistComponent implements OnInit {
-  items: [Item];
-  scrollNumber: number;
+	items: [Item];
+	scrollNumber: number;
 
-  onScroll() {
-    this.dataService.getTopListItems(this.scrollNumber).subscribe(items => {
-      for (let i = 0; i < items.length; i++) {
-        this.items.push(items[i]);
-      }
-      this.scrollNumber += 1;
-    });
+	onScroll() {
+		this.dataService.getTopListItems(this.scrollNumber).subscribe(items => {
+			for (let i = 0; i < items.length; i++) {
+				this.items.push(items[i]);
+			}
+			this.scrollNumber += 1;
+		});
+	}
 
-  }
+	constructor(private dataService: DataService) {
+		this.scrollNumber = 1;
+	}
 
-  constructor(private dataService: DataService) {
-    this.scrollNumber = 1;
-  }
-
-  ngOnInit() {
-    this.dataService.getTopListItems(0).subscribe(items => {
-      this.items = items;
-    });
-  }
-
+	ngOnInit() {
+		this.dataService.getTopListItems(1).subscribe(items => {
+			this.items = items;
+		});
+	}
 }
--- client/src/app/services/data.service.ts
+++ client/src/app/services/data.service.ts
@@ -1,30 +1,41 @@
-import { Injectable } from '@angular/core';
-import {Http} from '@angular/http';
-import 'rxjs/add/operator/map';
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+import "rxjs/add/operator/map";
 
 @Injectable()
 export class DataService {
+	itemsPerPage = 25;
 
-  constructor(public http: Http) {
-    console.log('Data service connected');
-   }
+	constructor(public http: Http) {
+		console.log("Data service connected");
+	}
 
-   getTopListItems(page) {
-    return this.http.get(`http://api.jowies.com/top/${page}`)
-    .map(res => res.json());
-  }
-  getCategory(category, page) {
-    return this.http.get(`http://api.jowies.com/${category}/${page}`)
-    .map(res => res.json());
-  }
+	getTopListItems(page) {
+		const to = page * this.itemsPerPage;
+		const from = page * this.itemsPerPage - this.itemsPerPage;
+		console.log(to);
+		console.log(from);
+		return this.http
+			.get(
+				`http://it2810-09.idi.ntnu.no:3000/products?from=${from}&limit=${to}&sort=-apk`
+			)
+			.map(res => res.json());
+	}
+	getCategory(category, page) {
+		const to = page * this.itemsPerPage;
+		const from = page * this.itemsPerPage - this.itemsPerPage;
+		return this.http
+			.get(
+				`http://it2810-09.idi.ntnu.no:3000/products?category=${category}&from=${from}&limit=${to}&sort=-apk`
+			)
+			.map(res => res.json());
+	}
 
-  getSpecificItem(id) {
-    //id = "5060154910315"
-    console.log(this.http.get(`http://api.jowies.com/barcode/${id}`));
-   return this.http.get(`http://api.jowies.com/barcode/${id}`)
-   .map(res => res.json());
-
- }
-
-
+	getSpecificItem(id) {
+		// id = "5060154910315"
+		console.log(this.http.get(`http://api.jowies.com/barcode/${id}`));
+		return this.http
+			.get(`http://api.jowies.com/barcode/${id}`)
+			.map(res => res.json());
+	}
 }
