8c42d8f23b0e2bd865c72a26e5f0e281da511954
marifred <fredriksenmari@gmail.com>
2017-11-04 15:49:08 +0100

    Add more locations

--- client/src/app/services/data.service.ts
+++ client/src/app/services/data.service.ts
@@ -13,5 +13,8 @@ export class DataService {
     return this.http.get(`http://api.jowies.com/top/${page}`)
     .map(res => res.json());
   }
+  getMap() {
+   return this.http.get("https://mapsearch.eniro.com/search/search.json?callback=jQuery211014733662236314338_1509801093491&profile=no&index=yp_sp&q=a%20s%20vinmonopolet&BBOX=4.21875%2C56.96893619436121%2C12.073974609375%2C62.865168668923125&pageSize=500");
+ }
 
 }
